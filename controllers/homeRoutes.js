bb201edb0c3127514f1852621d77e07ff52872f2
Chris <Ckratz17@gmail.com>
2024-03-20 15:57:25 -0400

    added handlebars for home, login, and signup pages

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,35 +5,36 @@ router.get('/', async (req, res) => {
     try {
         const userData =  await User.findAll()
       
-        res.status(200).json(userData)
+        res.render('home')
         
     } catch (err) {
         res.status(500).json
     }
 })
 
-// router.get('/login', async (req, res) => {
-//     try {
-//         if(req.session.logged_in) {
-//             res.redirect('/')
-//             return
-//         }
-//         res.render('login')
-//     } catch (err) {
-//         res.status(500).json(err)
-//     }
-// })
+router.get('/login', async (req, res) => {
+    try {
+        if(req.session.logged_in) {
+            res.redirect('/')
+            return
+        }
+        res.render('login')
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
-// router.get('/signup', async (req, res) => {
-//     try {
-//         if(req.session.logged_in) {
-//             res.redirect('/')
-//             return
-//         }
-//     } catch (err) {
-//         res.status(500).json(err)
-//     }
-// })
+router.get('/signup', async (req, res) => {
+    try {
+        if(req.session.logged_in) {
+            res.redirect('/')
+            return
+        }
+        res.render('signup')
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
 
 module.exports = router
